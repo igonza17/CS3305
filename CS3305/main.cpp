4d2468ba6df7879d5d4fcca6b94c1e390473de94
Ivan <ivanxgv@gmial.com>
2020-01-06 19:21:53 -0500

    Initial Commit

---
+++ CS3305/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS3305
+//
+//  Created by Ivan Gonzalez on 1/6/20.
+//  Copyright © 2020 Ivan Gonzalez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
